174c45827d8b1db8d9d78f6f9f8ae8012e060c22
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2023-01-04 19:10:56 +0100

    Klasse "Program" hinzugefügt

---
+++ src/syntaxtree/Program.java
@@ -0,0 +1,11 @@
+package syntaxtree;
+
+import java.util.Vector;
+
+public class Program {
+    private final Vector<Class> classes;
+
+    public Program(Vector<Class> classes) {
+        this.classes = classes;
+    }
+}
