619d9b6eca1e19d4a3239d2154ba8acfc832db28
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2023-01-16 13:15:27 +0100

    implemented local variables & refactoring

--- src/codegen/ExpressionGenerator.java
+++ src/codegen/ExpressionGenerator.java
@@ -3,102 +3,108 @@ package codegen;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import syntaxtree.Method;
 import syntaxtree.expressions.*;
 import syntaxtree.statements.IfStmt;
 
+import static codegen.StatementExpressionGenerator.genStmtExpr;
+import static codegen.StatementGenerator.*;
+import static codegen.ClassGenerator.fieldDescriptor;
+
+
 public class ExpressionGenerator {
-    public static void genExpr(Expression exp, MethodVisitor mv)
+    public static void genExpr(Expression exp, Method m)
     {
         switch (exp) {
             case IntegerExpr e:
                 switch (e.i) {
-                    case -1 -> mv.visitInsn(Opcodes.ICONST_M1);
-                    case 0 -> mv.visitInsn(Opcodes.ICONST_0);
-                    case 1 -> mv.visitInsn(Opcodes.ICONST_1);
-                    case 2 -> mv.visitInsn(Opcodes.ICONST_2);
-                    case 3 -> mv.visitInsn(Opcodes.ICONST_3);
-                    case 4 -> mv.visitInsn(Opcodes.ICONST_4);
-                    case 5 -> mv.visitInsn(Opcodes.ICONST_5);
-                    default -> mv.visitLdcInsn(e.i);
+                    case -1 -> m.visitor.visitInsn(Opcodes.ICONST_M1);
+                    case 0 -> m.visitor.visitInsn(Opcodes.ICONST_0);
+                    case 1 -> m.visitor.visitInsn(Opcodes.ICONST_1);
+                    case 2 -> m.visitor.visitInsn(Opcodes.ICONST_2);
+                    case 3 -> m.visitor.visitInsn(Opcodes.ICONST_3);
+                    case 4 -> m.visitor.visitInsn(Opcodes.ICONST_4);
+                    case 5 -> m.visitor.visitInsn(Opcodes.ICONST_5);
+                    default -> m.visitor.visitLdcInsn(e.i);
                 }
                 break;
             case CharExpr e:
-                mv.visitLdcInsn(e);
+                m.visitor.visitLdcInsn(e);
                 break;
             case BoolExpr e:
-                if (e.b) { mv.visitInsn(Opcodes.ICONST_1); }
-                else { mv.visitInsn(Opcodes.ICONST_0); }
+                if (e.b) { m.visitor.visitInsn(Opcodes.ICONST_1); }
+                else { m.visitor.visitInsn(Opcodes.ICONST_0); }
                 break;
             case StringExpr e:
-                mv.visitLdcInsn(e.s);
+                m.visitor.visitLdcInsn(e.s);
                 break;
             case JNullExpr e:
-                mv.visitInsn(Opcodes.ACONST_NULL);
+                m.visitor.visitInsn(Opcodes.ACONST_NULL);
                 break;
             case LocalOrFieldVarExpr e:
-                genLocalOrFieldVarExpr(e, mv);
+                genLocalOrFieldVarExpr(e, m);
                 break;
             case InstVarExpr e:
                 //TODO
                 break;
             case BinaryExpr e:
-                genBinaryExpr(e, mv);
+                genBinaryExpr(e, m);
                 break;
             case UnaryExpr e:
-                genUnaryExpr(e, mv);
+                genUnaryExpr(e, m);
                 break;
             case ThisExpr e:
-                mv.visitVarInsn(Opcodes.ALOAD, 0);
+                m.visitor.visitVarInsn(Opcodes.ALOAD, 0);
                 break;
             case SuperExpr e:
                 //TODO
                 break;
             case StmtExprExpr e:
-                //TODO
+                genStmtExpr(e.stmtExprExpr, m);
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + exp);
         }
     }
 
-    private static void genBinaryExpr(BinaryExpr expr, MethodVisitor mv) {
+    private static void genBinaryExpr(BinaryExpr expr, Method m) {
         String type = "int"; //TODO: get type from expr
         switch (type){
             case "int", "char":
                 switch (expr.eval) {
                     case "+" -> {
-                        genExpr(expr.expr1, mv);
-                        genExpr(expr.expr2, mv);
-                        mv.visitInsn(Opcodes.IADD);
+                        genExpr(expr.expr1, m);
+                        genExpr(expr.expr2, m);
+                        m.visitor.visitInsn(Opcodes.IADD);
                     }
                     case "-" -> {
-                        genExpr(expr.expr1, mv);
-                        genExpr(expr.expr2, mv);
-                        mv.visitInsn(Opcodes.ISUB);
+                        genExpr(expr.expr1, m);
+                        genExpr(expr.expr2, m);
+                        m.visitor.visitInsn(Opcodes.ISUB);
                     }
                     case "*" -> {
-                        genExpr(expr.expr1, mv);
-                        genExpr(expr.expr2, mv);
-                        mv.visitInsn(Opcodes.IMUL);
+                        genExpr(expr.expr1, m);
+                        genExpr(expr.expr2, m);
+                        m.visitor.visitInsn(Opcodes.IMUL);
                     }
                     case "/" -> {
-                        genExpr(expr.expr1, mv);
-                        genExpr(expr.expr2, mv);
-                        mv.visitInsn(Opcodes.IDIV);
+                        genExpr(expr.expr1, m);
+                        genExpr(expr.expr2, m);
+                        m.visitor.visitInsn(Opcodes.IDIV);
                     }
                 }
                 break;
             case "boolean":
                 switch (expr.eval) {
                     case "and","&&" -> {
-                        genExpr(expr.expr1, mv);
-                        genExpr(expr.expr2, mv);
-                        mv.visitInsn(Opcodes.IAND);
+                        genExpr(expr.expr1, m);
+                        genExpr(expr.expr2, m);
+                        m.visitor.visitInsn(Opcodes.IAND);
                     }
                     case "or","||" -> {
-                        genExpr(expr.expr1, mv);
-                        genExpr(expr.expr2, mv);
-                        mv.visitInsn(Opcodes.IOR);
+                        genExpr(expr.expr1, m);
+                        genExpr(expr.expr2, m);
+                        m.visitor.visitInsn(Opcodes.IOR);
                     }
                 }
                 break;
@@ -123,41 +129,59 @@ public class ExpressionGenerator {
         }
     }
 
-    private static void genUnaryExpr(UnaryExpr expr, MethodVisitor mv){
+    private static void genUnaryExpr(UnaryExpr expr, Method m){
         switch (expr.eval){
             case "-":
-                genExpr(expr.expr, mv);
-                mv.visitInsn(Opcodes.INEG);
+                genExpr(expr.expr, m);
+                m.visitor.visitInsn(Opcodes.INEG);
                 break;
             case "not","!":
                 Label retzero = new Label();
                 Label end = new Label();
 
-                genExpr(expr.expr, mv);
+                genExpr(expr.expr, m);
 
-                mv.visitJumpInsn(Opcodes.IFNE, retzero);
-                mv.visitInsn(Opcodes.ICONST_1);
-                mv.visitJumpInsn(Opcodes.GOTO, end);
+                m.visitor.visitJumpInsn(Opcodes.IFNE, retzero);
+                m.visitor.visitInsn(Opcodes.ICONST_1);
+                m.visitor.visitJumpInsn(Opcodes.GOTO, end);
 
-                mv.visitLabel(retzero);
-                mv.visitInsn(Opcodes.ICONST_0);
+                m.visitor.visitLabel(retzero);
+                m.visitor.visitInsn(Opcodes.ICONST_0);
 
-                mv.visitLabel(end);
+                m.visitor.visitLabel(end);
                 break;
         }
     }
 
 
-    private static void genLocalOrFieldVarExpr(LocalOrFieldVarExpr var, MethodVisitor mv) {
+    private static void genLocalOrFieldVarExpr(LocalOrFieldVarExpr var, Method m) {
         String type = "asdf"; //TODO: get type from expression
 
         //TODO:
         // look for variable names in parameters, locals and fields
         // a name cannot occur both in locals and parameters!
         // this means, that the method at hand has to know about the fields of the class, the method parameters and the local variables
+    }
 
+    private static void genLocalVar(LocalVar v, Method m) {
+        String type = "int"; //TODO get type from expression
+        int index = m.localVariableIndexes.get(v.name);
+
+        switch (type) {
+            case "int", "char", "boolean" -> {
+                m.visitor.visitVarInsn(Opcodes.ILOAD, index);
+            }
+            case default -> {
+                m.visitor.visitVarInsn(Opcodes.ALOAD, index);
+            }
+        }
 
     }
 
+    private static void genFieldVar(FieldVar f, Method m) {
+        String type = "int"; //TODO get type from expression
+
+        //m.visitor.visitFieldInsn(Opcodes.GETFIELD, f.owner, f.name, fieldDescriptor(f.type));
+    }
 
 }
