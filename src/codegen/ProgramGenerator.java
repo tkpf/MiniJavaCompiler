1febc1e87ea47c56e39d688803d961b10b994f46
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2023-01-21 12:09:45 +0100

    added program generator

--- src/codegen/ProgramGenerator.java
+++ src/codegen/ProgramGenerator.java
@@ -1,6 +1,8 @@
 package codegen;
 
 import syntaxtree.Class;
+import syntaxtree.Examples;
+import syntaxtree.Program;
 
 import java.io.IOException;
 import java.util.Vector;
@@ -9,9 +11,13 @@ import static codegen.ClassGenerator.cw2file;
 import static codegen.ClassGenerator.generateClassCode;
 
 public class ProgramGenerator {
-    public static void generateProgram(Vector<Class> classes) throws IOException {
-        for (Class c : classes) {
+    public static void generateProgram(Program program) throws IOException {
+        for (Class c : program.classes) {
             cw2file(generateClassCode(c));
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        generateProgram(Examples.program1);
     }
 }
