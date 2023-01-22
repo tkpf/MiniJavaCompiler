56a5271e6c6cf2445ddb501dc41ac8eb39bf6791
Kim Schuster <kim.schuster@student.uni-tuebingen.de>
2023-01-22 14:33:22 +0100

    Fixed `type` in `Statement` class.

--- src/syntaxtree/statements/Statement.java
+++ src/syntaxtree/statements/Statement.java
@@ -1,10 +1,11 @@
 package syntaxtree.statements;
 
+import syntaxtree.expressions.Type;
 import syntaxtree.statements.*;
 
 public abstract sealed class Statement permits BlockStmt, ReturnStmt, IfStmt, WhileStmt, LocalVarDeclStmt, StmtExprStmt {
 
-        public String type = "";
+        public Type type;
         private void codeGen() {
             //todo implement codeGen
         }
