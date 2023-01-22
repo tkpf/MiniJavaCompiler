7b96d3745d6cb02fbf97f8f78562fe322cc0c220
Kim Schuster <kim.schuster@student.uni-tuebingen.de>
2023-01-22 12:18:15 +0100

    Added `type: Type` field to sealed classes of AST.

--- src/syntaxtree/expressions/Expression.java
+++ src/syntaxtree/expressions/Expression.java
@@ -3,6 +3,8 @@ package syntaxtree.expressions;
 public abstract sealed class Expression
         permits StringExpr, BinaryExpr, BoolExpr, CharExpr, InstVarExpr, IntegerExpr, JNullExpr, LocalOrFieldVarExpr, StmtExprExpr, SuperExpr, ThisExpr, UnaryExpr, LocalVar, FieldVar {
 
+    public Type type;
+
     public void codeGen() {
         //todo implement codeGen
     }
--- src/syntaxtree/statements/Statement.java
+++ src/syntaxtree/statements/Statement.java
@@ -4,6 +4,7 @@ import syntaxtree.statements.*;
 
 public abstract sealed class Statement permits BlockStmt, ReturnStmt, IfStmt, WhileStmt, LocalVarDeclStmt, StmtExprStmt {
 
+        public String type = "";
         private void codeGen() {
             //todo implement codeGen
         }
