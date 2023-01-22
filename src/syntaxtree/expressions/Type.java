4acd136c3a2cb52b0c82e4417a9928b707c16120
Kim Schuster <kim.schuster@student.uni-tuebingen.de>
2023-01-22 12:03:21 +0100

    Removed `final` from Type.name and implemented `equals` method.

--- src/syntaxtree/expressions/Type.java
+++ src/syntaxtree/expressions/Type.java
@@ -2,10 +2,22 @@ package syntaxtree.expressions;
 
 public final class Type {
 
-    public final String name;
+    public String name;
 
     public Type(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof Type)) return false;
+        Type t = (Type) obj;
+        return this.name.equals(t.name);
+    }
+
+    public boolean equals(String s) {
+        return this.name.equals(s);
     }
 
     @Override
