277a4c875d62e869b77ebcfd643b5b61254c7abc
tkpf <tillkopff@gmail.com>
2023-01-24 15:40:49 +0100

    updated main_test, commenting .toString

--- test/Main_test.java
+++ test/Main_test.java
@@ -1,9 +1,11 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import parser.adapter.ClassAdapter;
 import parser.adapter.ProgramAdapter;
 import parser.production.JavaMiniLexer;
 import parser.production.JavaMiniParser;
+import syntaxtree.Class;
 import syntaxtree.Program;
 
 import java.io.IOException;
@@ -13,15 +15,18 @@ public class Main_test {
     public static void main(String[] args) throws IOException  {
 
         System.out.println("Running Main_test");
-        CharStream input = CharStreams.fromFileName("./test/test.txt");
+        CharStream input = CharStreams.fromFileName("test\\test.txt");
         JavaMiniLexer lexer = new JavaMiniLexer(input);
         JavaMiniParser parser = new JavaMiniParser(new CommonTokenStream(lexer));
 
-        System.out.print("parser output: ");
+        Program prgm = ProgramAdapter.adapt(parser.compilationUnit());
+        System.out.println("Abstract syntax was generated!");
+
+        // If you want to have a StringTree output in the console please comment the 2 above lines when constructing object prgm, because this makes the parser object unusable.
+        //System.out.print("parser output: ");
         //System.out.println(parser.compilationUnit().toStringTree(parser));
 
-        Program prgm = ProgramAdapter.adapt(parser.compilationUnit());
-        System.out.println(prgm);
+
 
     }
 
