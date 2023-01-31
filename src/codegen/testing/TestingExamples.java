20ed7b2fcb55830b2610007edd871ad4588272af
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2023-01-31 11:32:49 +0100

    updated tests

--- src/codegen/testing/TestingExamples.java
+++ src/codegen/testing/TestingExamples.java
@@ -1,8 +1,24 @@
 package codegen.testing;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import parser.adapter.ProgramAdapter;
+import parser.production.JavaMiniLexer;
+import parser.production.JavaMiniParser;
+import syntaxtree.Program;
+import typecheck.TypeCheck;
+import typecheck.exceptions.*;
+
+import java.io.IOException;
+
+import static codegen.ProgramGenerator.generateProgram;
+
 public class TestingExamples {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, MissingSymbolException, AlreadyDefinedException, TypeMismatchException {
+        /*
         TestClass t = new TestClass();
 
         t.storeThings(1, 'Q', false, "foo");
@@ -12,7 +28,16 @@ public class TestingExamples {
         System.out.println(t.sumUp(5));
         System.out.println(t.sumUpRecursive(5));
         System.out.println(t.testStore());
+        */
 
+        CharStream input = CharStreams.fromFileName("./src/codegen/testing/ExamplesToTest.txt");
+        JavaMiniLexer lexer = new JavaMiniLexer(input);
+        JavaMiniParser parser = new JavaMiniParser(new CommonTokenStream(lexer));
+        Program prgm = ProgramAdapter.adapt(parser.compilationUnit());
+        TypeCheck typeCheck = new TypeCheck(prgm);
+        typeCheck.check();
+        System.out.print(prgm);
 
+        generateProgram(prgm);
     }
 }
