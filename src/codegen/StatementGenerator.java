bca1ee601fca45c6f1297a9c733bc223cf00a2fe
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2023-02-11 18:44:48 +0100

    add empty return statements at the end of void blocks

--- src/codegen/StatementGenerator.java
+++ src/codegen/StatementGenerator.java
@@ -3,7 +3,9 @@ package codegen;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.Opcodes;
 import syntaxtree.Method;
+import syntaxtree.Type;
 import syntaxtree.expressions.Expression;
+import syntaxtree.expressions.JNullExpr;
 import syntaxtree.statements.*;
 import static codegen.StatementExpressionGenerator.genStmtExpr;
 import static codegen.ExpressionGenerator.*;
@@ -12,29 +14,25 @@ public class StatementGenerator {
     public static void genStmt(Statement stmt, Method m)
     {
         switch (stmt) {
-            case null:
-                m.visitor.visitInsn(Opcodes.NOP);
-                break;
-            case BlockStmt s:
+            case null -> m.visitor.visitInsn(Opcodes.NOP);
+            case BlockStmt s -> {
                 s.stmtBlck.forEach(b -> genStmt(b, m)); // todo: delete local variables after block is closed
-                break;
-            case ReturnStmt s:
-                genReturnStmt(s.rExpr, m);
-                break;
-            case VarDeclStmt s:
-                m.localVariableIndexes.put(s.name, m.localVariableIndexes.size()+1);
-                break;
-            case IfStmt s:
-                genIfStmt(s, m);
-                break;
-            case WhileStmt s:
-                genWhileStmt(s, m);
-                break;
-            case StmtExprStmt s:
-                genStmtExpr(s.stmtExpr, m);
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + stmt);
+
+                // adding 'return;' at the end of void-blocks in case they are not explicit in the code (since java allows to omit them)
+                if (s.stmtBlck.isEmpty() ||
+                        (s.type.name == "void" && s.stmtBlck.lastElement().getClass() != ReturnStmt.class))
+                {
+                    Expression emptyVoidRexpr = new JNullExpr(); //TODO: the JNUll is just a dummy that shouldn't be read
+                    emptyVoidRexpr.type = new Type("void");
+                    genReturnStmt(emptyVoidRexpr, m);
+                }
+            }
+            case ReturnStmt s -> genReturnStmt(s.rExpr, m);
+            case VarDeclStmt s -> m.localVariableIndexes.put(s.name, m.localVariableIndexes.size() + 1);
+            case IfStmt s -> genIfStmt(s, m);
+            case WhileStmt s -> genWhileStmt(s, m);
+            case StmtExprStmt s -> genStmtExpr(s.stmtExpr, m);
+            default -> throw new IllegalStateException("Unexpected value: " + stmt);
         }
 
     }
@@ -49,6 +47,9 @@ public class StatementGenerator {
             case "String", "null" -> {
                 genExpr(rexpr, m);
                 m.visitor.visitInsn(Opcodes.ARETURN);
+            }
+            case "void" -> {
+                m.visitor.visitInsn(Opcodes.RETURN);
             }
         }
     }
@@ -81,4 +82,5 @@ public class StatementGenerator {
 
         m.visitor.visitLabel(end);
     }
+
 }
