f5e591c2e32b9898da59453de6a79f063fbd503c
Matthias Wallner <m.wallner@student.uni-tuebingen.de>
2023-02-11 18:44:04 +0100

    updated assignment and method call

--- src/codegen/StatementExpressionGenerator.java
+++ src/codegen/StatementExpressionGenerator.java
@@ -15,6 +15,8 @@ import static codegen.ExpressionGenerator.genExpr;
 public class StatementExpressionGenerator {
     public static void genStmtExpr(StatementExpression stx, Method m) {
         switch (stx){
+            case null:
+                break;
             case AssignStmtExpr s:
                 genAssign(s, m);
                 break;
@@ -28,38 +30,35 @@ public class StatementExpressionGenerator {
     }
 
     public static void genAssign(AssignStmtExpr s, Method m) {
-        genExpr(s.asFromExpr, m);
         String type = s.as2Expr.type.name;
 
-        switch (s.as2Expr) {
-            case LocalOrFieldVarExpr var:
-
-                switch (var.context){
-                    case local -> {
-                        int index = m.localVariableIndexes.get(var.name);
-
-                        switch (type) {
-                            case "int", "char", "boolean" -> {
-                                m.visitor.visitVarInsn(Opcodes.ISTORE, index);
-                            }
-                            case default -> {
-                                m.visitor.visitVarInsn(Opcodes.ASTORE, index);
-                            }
+        if (s.as2Expr instanceof LocalOrFieldVarExpr var) {
+            switch (var.context) {
+                case local -> {
+                    int index = m.localVariableIndexes.get(var.name);
+                    genExpr(s.asFromExpr, m);
+                    switch (type) {
+                        case "int", "char", "boolean" -> {
+                            m.visitor.visitVarInsn(Opcodes.ISTORE, index);
                         }
-                        break;
+                        case default -> {
+                            m.visitor.visitVarInsn(Opcodes.ASTORE, index);
+                        }
                     }
-                    case field -> {
-                        m.visitor.visitFieldInsn(Opcodes.PUTFIELD, m.ownerClass.name, var.name, fieldDescriptor(var.type.name));
-                        break;
-                    }
-                    case unknown -> {
-                        throw new IllegalStateException("variable unknown");
-                    }
+                    break;
                 }
-
-                break;
-            default:
-                throw new IllegalStateException("cannot assign to anything other than LocalOrFieldVarExpr: " + s.as2Expr);
+                case field -> {
+                    m.visitor.visitVarInsn(Opcodes.ALOAD, 0);
+                    genExpr(s.asFromExpr, m);
+                    m.visitor.visitFieldInsn(Opcodes.PUTFIELD, m.ownerClass.name, var.name, fieldDescriptor(var.type.name));
+                    break;
+                }
+                case unknown -> {
+                    throw new IllegalStateException("variable unknown");
+                }
+            }
+        } else {
+            throw new IllegalStateException("cannot assign to anything other than LocalOrFieldVarExpr: " + s.as2Expr);
         }
     }
 
@@ -75,6 +74,7 @@ public class StatementExpressionGenerator {
     }
 
     public static void genMethodCall(MethodCallStmtExpr mcall, Method m) {
+        genExpr(mcall.obj, m);
         mcall.methParams.forEach(e -> genExpr(e, m));
         m.visitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, mcall.obj.type.name, mcall.meth, methodDescriptor(m), false);
     }
