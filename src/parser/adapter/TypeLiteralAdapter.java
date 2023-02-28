417cebde1838c7cb31207f192d5f135d659a0c26
tkpf <69465288+tkpf@users.noreply.github.com>
2023-02-28 22:49:04 +0100

    unescape String sequences

--- src/parser/adapter/TypeLiteralAdapter.java
+++ src/parser/adapter/TypeLiteralAdapter.java
@@ -21,7 +21,12 @@ public class TypeLiteralAdapter {
                     Integer.parseInt(ctx.DecimalLiteral().getText()));
         }
         else if (ctx.StringLiteral() != null) {
-            return new StringExpr(ctx.StringLiteral().getText());
+
+            String originalString = ctx.StringLiteral().getText();
+            String stringWithoutQuotes = originalString.substring(1, originalString.length() - 1);
+            // Unescape Java escape sequences in string
+            String unescapedString = unescapeJava(stringWithoutQuotes);
+            return new StringExpr(unescapedString);
         }
         else if (ctx.NullLiteral() != null) {
             return new JNullExpr();
@@ -31,5 +36,42 @@ public class TypeLiteralAdapter {
             throw new EscapeHatchException();
         }
     }
+
+    public static String unescapeJava(String str) {
+        if (str == null) {
+            return null;
+        }
+        int len = str.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if (c == '\\') {
+                if (i < len - 1) {
+                    char nextChar = str.charAt(i + 1);
+                    switch (nextChar) {
+                        case '\"' -> sb.append('\"');
+                        case '\'' -> sb.append('\'');
+                        case '\\' -> sb.append('\\');
+                        case 'r' -> sb.append('\r');
+                        case 'n' -> sb.append('\n');
+                        case 'f' -> sb.append('\f');
+                        case 't' -> sb.append('\t');
+                        case 'b' -> sb.append('\b');
+                        default -> {
+                            sb.append('\\');
+                            sb.append(nextChar);
+                        }
+                    }
+                    i++;
+                } else {
+                    sb.append('\\');
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
 
 }
